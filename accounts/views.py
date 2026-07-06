f86ec37ac2a80bd616ba67032955d714468a484e
iDadabaev <home@homes-MacBook-Pro.local>
2026-07-06 15:43:01 +0500

    add register form

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.contrib import messages
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Akkaunt muvaffaqiyatli yaratildi! {username}, tizimga kirish mumkin.")
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'accounts/register.html', {'form': form})
+
