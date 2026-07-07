0982b83fd2d499027d0cbbf877476570d36e90a4
Alisher <dadabaev027@gmail.com>
2026-07-07 22:08:18 +0500

    Add search function

--- products/views.py
+++ products/views.py
@@ -9,6 +9,10 @@ def product_list(request, category_slug=None):
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
 
+    query = request.GET.get('search')
+    if query:
+        products = products.filter(name__icontains=query)
+
     if category_slug:
 
         category = get_object_or_404(Category, slug = category_slug)
@@ -18,7 +22,8 @@ def product_list(request, category_slug=None):
     context = {
         'category': category,
         'categories': categories,
-        'products': products
+        'products': products,
+        'query': query
         }
 
     return render(request, 'products/product/list.html', context)
